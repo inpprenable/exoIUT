f03b9c57e394cab16de98e219b2a884b752dca35
Robert <robert@leffe.com>
2026-04-09 15:22:04 +0200

    ajout de la fonction thomas_sort

--- project/main.py
+++ project/main.py
@@ -4,23 +4,19 @@ from lib import evaluate
 def example_sort(arr: list[int]) -> list[int]:
     return sorted(arr)
 
-
 # ===============================
 # Execution
 # ===============================
 
-if __name__ == "__main__":
-    for name, func in SOLUTIONS:
-        evaluate(func, name)
-
-def thomas_sort(arr: list[int]) -> list[int]:
-    return sorted(arr)
-
-
-
-
+def thomas_sort(arr_t: list[int]) -> list[int]:
+    return sorted(arr_t)
 
 SOLUTIONS = [
     ("example", example_sort),("thomas", thomas_sort)
 ]
+
+
+if __name__ == "__main__":
+    for name, func in SOLUTIONS:
+        evaluate(func, name)
 
