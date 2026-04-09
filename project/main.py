b9a4bee588c41a8207c7b8c03fbbe0f5cb1a9aa8
Adrien <adrien.bouharati@gmail.com>
2026-04-09 16:31:02 +0200

    triAdrien
    TriABulle

--- project/main.py
+++ project/main.py
@@ -24,6 +24,20 @@ def fonction_idrys(arr: list[int]) -> list[int]:
                 nouvelle_liste[j + 1] = temp
     return nouvelle_liste
 
+def fonction_adrien(arr: list[int]) -> list[int]:
+    nouvelle_liste = []
+    for nombre in arr:
+        nouvelle_liste.append(nombre)
+    taille = len(nouvelle_liste)
+    for i in range(taille):
+        for j in range(0, taille - i - 1):
+            if nouvelle_liste[j] > nouvelle_liste[j + 1]:
+                temp = nouvelle_liste[j]
+                nouvelle_liste[j] = nouvelle_liste[j + 1]
+                nouvelle_liste[j + 1] = temp
+    return nouvelle_liste
+
+
 def trie_mathieu(arr_m: list[int]) -> list[int]:
 	return sorted(arr_m)
 
@@ -36,6 +50,7 @@ SOLUTIONS = [
     ("mathieu", trie_mathieu),
     ("lucas", tri_lucas),
     ("idrys", fonction_idrys)
+    ("adrien", foction_adrien)
 ]
 
 
