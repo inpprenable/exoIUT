e4c239b9ad5b7593275ded8be3399176cb92dd3e
Bi0n0x <belal.idrys55@gmail.com>
2026-04-09 15:54:03 +0200

    Ajout de ma fonction de tri

--- project/main.py
+++ project/main.py
@@ -11,6 +11,19 @@ def example_sort(arr: list[int]) -> list[int]:
 def thomas_sort(arr_t: list[int]) -> list[int]:
     return sorted(arr_t)
 
+def fonction_idrys(arr: list[int]) -> list[int]:
+    nouvelle_liste = []
+    for nombre in arr:
+        nouvelle_liste.append(nombre)
+    taille = len(nouvelle_liste)
+    for i in range(taille):
+        for j in range(0, taille - i - 1):
+            if nouvelle_liste[j] > nouvelle_liste[j + 1]:
+                temp = nouvelle_liste[j]
+                nouvelle_liste[j] = nouvelle_liste[j + 1]
+                nouvelle_liste[j + 1] = temp
+    return nouvelle_liste
+
 SOLUTIONS = [
     ("example", example_sort),("thomas", thomas_sort)
 ]
