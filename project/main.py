b1ce0c1e892216dd7e09c87e577d55e889fd013b
bob <bob@gmail.com>
2026-04-09 16:01:34 +0200

    Ajout de la partie de Lucas

--- project/main.py
+++ project/main.py
@@ -14,8 +14,11 @@ def thomas_sort(arr_t: list[int]) -> list[int]:
 def trie_mathieu(arr_m: list[int]) -> list[int]:
 	return sorted(arr_m)
 
+def tri_lucas(arr_lucas: list[int]) -> list[int]:
+    return sorted(arr_lucas)
+
 SOLUTIONS = [
-    ("example", example_sort),("thomas", thomas_sort),("mathieu", trie_mathieu)
+    ("example", example_sort),("thomas", thomas_sort),("mathieu", trie_mathieu),("lucas", tri_lucas)
 ]
 
 
